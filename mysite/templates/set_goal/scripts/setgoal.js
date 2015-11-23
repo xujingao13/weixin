508723e6cf0087ff1be0829e92bdaf846e184aa6
sheep94lion <sheep94lion@gmail.com>
2015-11-23 18:55:01 +0800

    the webpage to set goal

---
+++ mysite/templates/set_goal/scripts/setgoal.js
@@ -0,0 +1,9 @@
+var STEP_METER = 0.6;
+var update_dis = function () {
+	$('#goal_dis').val(Math.floor((Number($('#goal_step').val()) * STEP_METER).toString()));
+};
+var update_step = function () {
+	$('#goal_step').val(Math.floor((Number($('#goal_dis').val()) / STEP_METER).toString()));
+};
+$('#goal_step').on('input', update_dis);
+$('#goal_dis').on('input', update_step);
