19b97f75b3cf6be05665bdf59b218814e82bdd37
ClearSpirit <xujingao13@gmail.com>
2015-11-29 15:16:04 +0800

    Jingao models

--- mysite/mysite/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Record(models.Model):
-    user = models.CharField(max_length=30)
-    time = models.DateField()
-    step = models.IntegerField(max_length=60)
--- mysite/wechat/models.py
+++ mysite/wechat/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 
+
+class RingUser(models.Model):
+    user = models.CharField(max_length=30, primary_key=True)
+    sex = models.IntegerField()
+    age = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    last_record = models.IntegerField()
+
+
 class Record(models.Model):
-    user = models.CharField(max_length=30)
-    step = models.IntegerField()
-    time = models.DateTimeField(auto_now_add = True)
+    user_name = models.CharField(max_length=30)
+    startTime = models.IntegerField()
+    endTime = models.IntegerField()
+    type = models.IntegerField()
+    distance = models.IntegerField()
+    speed = models.FloatField()
+    calories = models.IntegerField()
+    steps = models.IntegerField()
+    subType = models.IntegerField()
+    actTime = models.IntegerField()
+    nonActTime = models.IntegerField()
+    dsNum = models.IntegerField()
+    lsNum = models.IntegerField()
+    wakeNum = models.IntegerField()
+    wakeTimes = models.IntegerField()
+    score = models.FloatField()
+
+
