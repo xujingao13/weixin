aba5a1e16de236b13bdd0f397e2d9a6252cc8935
ClearSpirit <xujingao13@gmail.com>
2015-12-04 19:37:39 +0800

    Jingao merge

--- wechat_treasure_ring/wechat_response/views.py
+++ wechat_treasure_ring/wechat_response/views.py
@@ -86,7 +86,7 @@ def weixin(request):
                     response = we_chat.response_news([{
                         'title': u'Today\'s amount of exercise',
                         'description': 'data analysis',
-                        'picurl': 'http://7xn2s5.com1.z0.glb.clouddn.com/ez.png',
+                        'picurl': 'http://7xn2s5.com1.z0.glb.clouddn.com/info.jpg',
                         'url': 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+AppID+'&redirect_uri=http%3a%2f%2f'+LOCAL_IP+'%2fsleepAnalysis.html'+'&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect'}])
                     return HttpResponse(response)
 
@@ -103,109 +103,6 @@ def get_openid(request, code):
     openid = json.loads(string_json)['openid']
     print openid
     return HttpResponse(json.dumps(openid))
-
-@csrf_exempt
-def get_chart(request, user, dayFlag):
-    data = Record.objects.filter(user=user)
-    i = 0
-    html = "noData.html"
-    length = len(data)
-    if length > 0:
-        last = length - 1
-        if dayFlag == "today":
-            day = data[last].time.day
-            html = "TodayChart.html"
-        elif dayFlag == "yesterday":
-            day = data[last].time.day - 2
-            html = "YesterdayChart.html"
-
-        flag = 0
-        #'start' variable is the item in the database which stores the data whose day is the variable 'day' first
-        while i < length:
-            if data[i].time.day == day and flag == 0:
-                start = i
-                flag = 1
-            elif data[i].time.day != day and flag == 1:
-                break;
-            i += 1
-        last = i - 1
-        #'last'variable is the item in the database which stores the data whose day is the variable 'day' last
-        all_steps = data[last].step - data[start].step
-        end_time = data[last].time.time().strftime("%H:%M:%S")
-        start_time = data[start].time.time().strftime("%H:%M:%S")
-        first_time = data[start].time
-        data = data[start:last]
-        info = []
-        length = len(data)
-        for i in range(0, length):
-            if i == 0:
-                info.append([0, data[0].step])
-            else:
-                info.append([(data[i].time - first_time).seconds, data[i].step - data[i - 1].step])
-        return render_to_response(html, {
-            "user": user,
-            "data": data,
-            "info": info,
-            "all_steps": all_steps,
-            "start_time": start_time,
-            "end_time": end_time,
-        }, context_instance=RequestContext(request))
-    else:
-        return render_to_response('noData.html', {
-            "user":user,
-        }, context_instance=RequestContext(request))
-
-
-def today_chart(request, user):
-    return get_chart(request, user, "today")
-
-
-def play_game(request):
-    return render_to_response('2048.html')
-
-
-def play_bird(request):
-    return render_to_response('bird.html')
-
-def yesterday_chart(request, user):
-    return get_chart(request, user, "yesterday")
-
-
-def last_week_chart(request, user):
-    data = Record.objects.filter(user=user)
-    info = []
-    i = 0
-    length = len(data)
-    if length > 0:
-        last = length - 1
-        last_today = data[last].time.day - 2
-        while i < length:
-            if data[i].time.day == last_today:
-                start_day = i
-                break
-            i += 1
-        all_steps = data[last].step - data[start_day].step
-        j = 0
-        i = start_day
-        day = last_today
-        while i < length and j < 7:
-            last_day_step = data[i].step
-            while data[i].time.day == day and i < length - 1:
-                i += 1
-            j += 1
-            day += 1
-            info.append([j, data[i].step - last_day_step])
-
-        return render_to_response('LastWeekChart.html', {
-            "user": user,
-            "info": info,
-            "data": data,
-            "all_steps": all_steps,
-        }, context_instance=RequestContext(request))
-    else:
-        return render_to_response('noData.html', {
-            "user": user,
-        }, context_instance=RequestContext(request))
 
 
 @csrf_exempt
